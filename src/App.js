4f4736eac934881bc98d851468c13a13c75f9128
Ambar Srivastava <67692421+ambar-srivastava@users.noreply.github.com>
2023-01-05 15:17:31 +0530

    added cryptocurrencies in footer tab

--- src/App.js
+++ src/App.js
@@ -40,6 +40,7 @@ const App = () => {
           <Space>
             <Link to="/">Home</Link>
             {/* <Link to="/exchanges">Exchanges</Link> */}
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
             <Link to="/news">News</Link>
           </Space>
         </footer>
